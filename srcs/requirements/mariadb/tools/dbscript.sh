758e954d5ba7e50b38734ea346b8aee46b9d2762
mudoh <mudoh@student.42.fr>
2024-06-14 03:15:46 +0200

    pid 1 faut ce renseigner trop long

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -2,13 +2,18 @@
 mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld 
 chown -R mysql:mysql /var/lib/mysql
 
+
 mysqld --user=mysql --bootstrap << EOF
+    FLUSH PRIVILEGES;
     CREATE DATABASE IF NOT EXISTS $DB_NAME;
-    CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-    GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASS';
-    SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
-    USE $DB_NAME;
+    GRANT ALL ON *.* TO 'root'@'$DB_HOST' IDENTIFIED BY '$DB_ROOT_PASS' WITH GRANT OPTION;
+    GRANT ALL ON *.* TO 'root'@'%' identified by '$DB_ROOT_PASS' WITH GRANT OPTION ;
+    GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' identified by '$DB_PASS' WITH GRANT OPTION ;
+    SET PASSWORD FOR 'mysql'@'%'=PASSWORD('${DB_ROOT_PASS}') ;
+    SET PASSWORD FOR 'root'@'%'=PASSWORD('${DB_ROOT_PASS}') ;
+    SET PASSWORD FOR 'root'@'$DBHOST'=PASSWORD('${DB_ROOT_PASS}') ;
+    SET PASSWORD FOR '$DB_USER'@'%' = PASSWORD('${DB_PASS}');
+    FLUSH PRIVILEGES;
 EOF
 
 exec mysqld_safe 
